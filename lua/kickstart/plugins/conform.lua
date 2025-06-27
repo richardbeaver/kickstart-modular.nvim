ca147703ac15ea856105a51df76db582528a1927
Richard Beaver <rickybeaver3@gmail.com>
2025-06-27 15:03:56 -0400

    Udpate formatting settings
    - Turn off formatting on save
    - Add markdown formatting
    - Trim whitespace and newlines in files with no other formatter
    configured

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -1,5 +1,26 @@
 return {
   { -- Autoformat
+    -- Using this to run formatting on <leader>f
+    -- To see all formatters:
+    -- :help conform-formatters
+    --
+    -- Original format on save configuration:
+    -- format_on_save = function(bufnr)
+    --   -- Disable "format_on_save lsp_fallback" for languages that don't
+    --   -- have a well standardized coding style. You can add additional
+    --   -- languages here or re-enable it for the disabled ones.
+    --   -- local disable_filetypes = { c = true, cpp = true }
+    --   local disable_filetypes = {}
+    --   if disable_filetypes[vim.bo[bufnr].filetype] then
+    --     return nil
+    --   else
+    --     return {
+    --       timeout_ms = 500,
+    --       lsp_format = 'fallback',
+    --     }
+    --   end
+    -- end,
+    --
     'stevearc/conform.nvim',
     event = { 'BufWritePre' },
     cmd = { 'ConformInfo' },
@@ -15,20 +36,6 @@ return {
     },
     opts = {
       notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = {} -- { c = true, cpp = true }
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          return nil
-        else
-          return {
-            timeout_ms = 500,
-            lsp_format = 'fallback',
-          }
-        end
-      end,
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
@@ -40,9 +47,16 @@ return {
         cpp = { 'clang-format' },
         c = { 'clang-format' },
         --
+        markdown = { 'mdformat' },
+        --
         -- Use the "_" filetype to run formatters on filetypes that don't
         -- have other formatters configured.
-        ['_'] = { 'trim_whitespace' },
+        ['_'] = { 'trim_whitespace', 'trim_newlines' },
+      },
+      formatters = {
+        mdformat = {
+          prepend_args = { '--wrap=80' },
+        },
       },
     },
   },
