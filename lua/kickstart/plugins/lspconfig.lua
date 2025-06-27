a99de9025afb1ea277965fa405d0c290b4194b63
Richard Beaver <rickybeaver3@gmail.com>
2025-06-27 15:36:03 -0400

    Set ensure installed with lsps I'm likely to use

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -211,11 +211,17 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        -- See `:help lspconfig-all` for a list of all the pre-configured LSPs
+        --
         clangd = {},
-        -- gopls = {},
+        rust_analyzer = {},
         pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+        --
+        cmake = {},
+        prettierd = {},
+        prettier = {},
+        markdownlint = {},
+        mdformat = {},
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
