30264c715f070defed650875d2788fff346f7298
Richard Beaver <rickybeaver3@gmail.com>
2025-06-30 11:37:09 -0400

    Re-enable format on save with conform.nvim

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -1,25 +1,8 @@
 return {
   { -- Autoformat
-    -- Using this to run formatting on <leader>f
+    --
     -- To see all formatters:
     -- :help conform-formatters
-    --
-    -- Original format on save configuration:
-    -- format_on_save = function(bufnr)
-    --   -- Disable "format_on_save lsp_fallback" for languages that don't
-    --   -- have a well standardized coding style. You can add additional
-    --   -- languages here or re-enable it for the disabled ones.
-    --   -- local disable_filetypes = { c = true, cpp = true }
-    --   local disable_filetypes = {}
-    --   if disable_filetypes[vim.bo[bufnr].filetype] then
-    --     return nil
-    --   else
-    --     return {
-    --       timeout_ms = 500,
-    --       lsp_format = 'fallback',
-    --     }
-    --   end
-    -- end,
     --
     'stevearc/conform.nvim',
     event = { 'BufWritePre' },
@@ -36,6 +19,30 @@ return {
     },
     opts = {
       notify_on_error = false,
+      -- Disabling function with unnecessary table check for now
+      -- format_on_save = function(bufnr)
+      --   -- Disable "format_on_save lsp_fallback" for languages that don't
+      --   -- have a well standardized coding style. You can add additional
+      --   -- languages here or re-enable it for the disabled ones.
+      --   -- local disable_filetypes = { c = true, cpp = true }
+      --   local disable_filetypes = {}
+      --   if disable_filetypes[vim.bo[bufnr].filetype] then
+      --     return nil
+      --   else
+      --     return {
+      --       timeout_ms = 500,
+      --       lsp_format = 'fallback',
+      --     }
+      --   end
+      -- end,
+      --
+      format_on_save = function()
+        return {
+          timeout_ms = 500,
+          lsp_format = 'fallback',
+        }
+      end,
+
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
